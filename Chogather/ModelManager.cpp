f858afd61879f56f59dc8440697705c2f28a3164
emdogie <marek.garczewski@gmail.com>
2020-08-16 19:47:13 +0200

    CHANGE ModelManager to work with models without textures and simplify code

--- Chogather/ModelManager.cpp
+++ Chogather/ModelManager.cpp
@@ -1,4 +1,8 @@
 #include "ModelManager.h"
+#include <fstream>
+#include <string>
+#include <sstream>
+#include <algorithm>
 using namespace std;
 
 Model::Model() {
@@ -10,50 +14,63 @@ bool Model::loadobj(const char * path)
     vector<glm::vec3> temp_vertices;
     vector<glm::vec2> temp_uvs;
     vector<glm::vec3> temp_normals;
-    FILE * file = fopen(path, "r");
-    if (file == NULL) {
-        printf("Cant open the file !\n");
-        return false;
-    }
-    while (1) {
-        char lineHeader[128];
-        // read the first word of the line
-        int res = fscanf(file, "%s", lineHeader);
-        if (res == EOF)
-            break;
-        if (strcmp(lineHeader, "v") == 0) {
+    ifstream file(path);
+    string line;
+    while (getline(file, line)) {
+        if (line.substr(0, line.find(" ")) == "v") {
+            line.erase(std::remove(line.begin(), line.end(), 'v'), line.end());
+            stringstream ss(line);
             glm::vec3 vertex;
-            fscanf(file, "%f %f %f\n", &vertex.x, &vertex.y, &vertex.z);
+            ss >> vertex.x >> vertex.y >> vertex.z;
             temp_vertices.push_back(vertex);
-        }
-        else if (strcmp(lineHeader, "vt") == 0) {
+        } 
+        else if (line.substr(0, line.find(" ")) == "vt") {
+            line.erase(std::remove(line.begin(), line.end(), 'v'), line.end());
+            line.erase(std::remove(line.begin(), line.end(), 't'), line.end());
+            stringstream ss(line);
             glm::vec2 uv;
-            fscanf(file, "%f %f\n", &uv.x, &uv.y);
+            ss >> uv.x >> uv.y;
             temp_uvs.push_back(uv);
         }
-        else if (strcmp(lineHeader, "vn") == 0) {
+        else if (line.substr(0, line.find(" ")) == "vn") {
+            line.erase(std::remove(line.begin(), line.end(), 'v'), line.end());
+            line.erase(std::remove(line.begin(), line.end(), 'n'), line.end());
+            stringstream ss(line);
             glm::vec3 normal;
-            fscanf(file, "%f %f %f\n", &normal.x, &normal.y, &normal.z);
+            ss >> normal.x >> normal.y >> normal.z;
             temp_normals.push_back(normal);
         }
-        else if (strcmp(lineHeader, "f") == 0) {
-            unsigned int vertexIndex[3], uvIndex[3], normalIndex[3];
-            int matches = fscanf(file, "%d/%d/%d %d/%d/%d %d/%d/%d\n", &vertexIndex[0], &uvIndex[0], &normalIndex[0], &vertexIndex[1], &uvIndex[1], &normalIndex[1], &vertexIndex[2], &uvIndex[2], &normalIndex[2]);
-            if (matches != 9) {
-                printf("File cant be read\n");
-                return false;
+        else if (line.substr(0, line.find(" ")) == "f") {
+            line.erase(std::remove(line.begin(), line.end(), 'f'), line.end());
+            if (line.find("//") != string::npos) { // model without textures
+                replace(line.begin(), line.end(), '/', ' ');
+                stringstream ss(line);
+                int temp;
+                while (ss) {
+                    ss >> temp;
+                    vertexIndices.push_back(temp);
+                    ss >> temp;
+                    normalIndices.push_back(temp);
+                }
             }
-            vertexIndices.push_back(vertexIndex[0]);
-            vertexIndices.push_back(vertexIndex[1]);
-            vertexIndices.push_back(vertexIndex[2]);
-            uvIndices.push_back(uvIndex[0]);
-            uvIndices.push_back(uvIndex[1]);
-            uvIndices.push_back(uvIndex[2]);
-            normalIndices.push_back(normalIndex[0]);
-            normalIndices.push_back(normalIndex[1]);
-            normalIndices.push_back(normalIndex[2]);
+            else { //model with textures
+                replace(line.begin(), line.end(), '/', ' ');
+                stringstream ss(line);
+                int temp;
+                while (!ss.eof()) {
+                    ss >> temp;
+                    vertexIndices.push_back(temp);
+                    ss >> temp;
+                    uvIndices.push_back(temp);
+                    ss >> temp;
+                    normalIndices.push_back(temp);
+                }
+            }
         }
+
+
     }
+
     for (unsigned int i = 0; i < vertexIndices.size(); i++) {
         unsigned int vertexIndex = vertexIndices[i];
         glm::vec3 vertex = temp_vertices[vertexIndex - 1];
